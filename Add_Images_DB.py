c22e2862606d2c8dd703a34deef28a35b722fb6c
lephuckhanggia <lephuckhanggia@gmail.com>
2024-09-07 09:25:58 +0700

    Set to defaut

--- Add_Images_DB.py
+++ Add_Images_DB.py
@@ -27,41 +27,19 @@ collection = client.get_or_create_collection(
     data_loader=data_loader
 )
 
-# Define batch size
-batch_size = 100
-
-
 def add_images_to_collection(folder_path):
-    batch_images = []
-    batch_ids = []
-
     for image_path in tqdm(image_files, desc="Creating Image Embeddings and Adding to DB"):
         try:
             image = np.array(Image.open(image_path))
             unique_id = os.path.relpath(image_path, start=image_folder_path)  # Use relative path as ID
-            batch_images.append(image)
-            batch_ids.append(unique_id)
-
-            # If batch is ready, add it to the collection
-            if len(batch_images) == batch_size:
-                collection.add(
-                    ids=batch_ids,
-                    images=batch_images
-                )
-                batch_images.clear()  # Clear batch after adding
-                batch_ids.clear()
-
+            collection.add(
+                ids=[unique_id],
+                images=[image]
+            )
         except Exception as e:
             print(f"Error processing {image_path}: {e}")
             traceback.print_exc()  # Optional: Print detailed traceback for debugging
 
-    # Add any remaining images if they don't make up a full batch
-    if batch_images:
-        collection.add(
-            ids=batch_ids,
-            images=batch_images
-        )
-
-
 add_images_to_collection(image_folder_path)
-print(f"Time to process: {timer() - start}")
+print(f"Time to process: {timer()-start}")
+
